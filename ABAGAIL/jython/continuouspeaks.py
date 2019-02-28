850498d082d92d2141b58b2f47502864fc0463c3
karankishinani <karan_kishinani@hotmail.com>
2019-02-28 12:49:34 -0500

    Initial Commit

---
+++ ABAGAIL/jython/continuouspeaks.py
@@ -0,0 +1,209 @@
+import sys
+import os
+import time
+
+import java.io.FileReader as FileReader
+import java.io.File as File
+import java.lang.String as String
+import java.lang.StringBuffer as StringBuffer
+import java.lang.Boolean as Boolean
+import java.util.Random as Random
+
+import dist.DiscreteDependencyTree as DiscreteDependencyTree
+import dist.DiscreteUniformDistribution as DiscreteUniformDistribution
+import dist.Distribution as Distribution
+import opt.DiscreteChangeOneNeighbor as DiscreteChangeOneNeighbor
+import opt.EvaluationFunction as EvaluationFunction
+import opt.GenericHillClimbingProblem as GenericHillClimbingProblem
+import opt.HillClimbingProblem as HillClimbingProblem
+import opt.NeighborFunction as NeighborFunction
+import opt.RandomizedHillClimbing as RandomizedHillClimbing
+import opt.SimulatedAnnealing as SimulatedAnnealing
+import opt.example.FourPeaksEvaluationFunction as FourPeaksEvaluationFunction
+import opt.ga.CrossoverFunction as CrossoverFunction
+import opt.ga.SingleCrossOver as SingleCrossOver
+import opt.ga.DiscreteChangeOneMutation as DiscreteChangeOneMutation
+import opt.ga.GenericGeneticAlgorithmProblem as GenericGeneticAlgorithmProblem
+import opt.ga.GeneticAlgorithmProblem as GeneticAlgorithmProblem
+import opt.ga.MutationFunction as MutationFunction
+import opt.ga.StandardGeneticAlgorithm as StandardGeneticAlgorithm
+import opt.ga.UniformCrossOver as UniformCrossOver
+import opt.prob.GenericProbabilisticOptimizationProblem as GenericProbabilisticOptimizationProblem
+import opt.prob.MIMIC as MIMIC
+import opt.prob.ProbabilisticOptimizationProblem as ProbabilisticOptimizationProblem
+import shared.FixedIterationTrainer as FixedIterationTrainer
+import opt.example.ContinuousPeaksEvaluationFunction as ContinuousPeaksEvaluationFunction
+from array import array
+from itertools import product
+from time import clock
+
+from base import *
+
+# Adapted from https://github.com/cmaron/CS-7641-assignments/blob/master/assignment2/continuouspeaks.py
+
+"""
+Commandline parameter(s):
+   none
+"""
+
+# N=60
+# T=N/10
+# fill = [2] * N
+# ranges = array('i', fill)
+#
+# ef = ContinuousPeaksEvaluationFunction(T)
+# odd = DiscreteUniformDistribution(ranges)
+# nf = DiscreteChangeOneNeighbor(ranges)
+# mf = DiscreteChangeOneMutation(ranges)
+# cf = SingleCrossOver()
+# df = DiscreteDependencyTree(.1, ranges)
+# hcp = GenericHillClimbingProblem(ef, odd, nf)
+# gap = GenericGeneticAlgorithmProblem(ef, odd, mf, cf)
+# pop = GenericProbabilisticOptimizationProblem(ef, odd, df)
+#
+# rhc = RandomizedHillClimbing(hcp)
+# fit = FixedIterationTrainer(rhc, 200000)
+# fit.train()
+# print "RHC: " + str(ef.value(rhc.getOptimal()))
+#
+# sa = SimulatedAnnealing(1E11, .95, hcp)
+# fit = FixedIterationTrainer(sa, 200000)
+# fit.train()
+# print "SA: " + str(ef.value(sa.getOptimal()))
+#
+# ga = StandardGeneticAlgorithm(200, 100, 10, gap)
+# fit = FixedIterationTrainer(ga, 1000)
+# fit.train()
+# print "GA: " + str(ef.value(ga.getOptimal()))
+#
+# mimic = MIMIC(200, 20, pop)
+# fit = FixedIterationTrainer(mimic, 1000)
+# fit.train()
+# print "MIMIC: " + str(ef.value(mimic.getOptimal()))
+
+N = 100
+T = 29
+
+maxIters = 5001
+numTrials = 5
+fill = [2] * N
+ranges = array('i', fill)
+outfile = OUTPUT_DIRECTORY + '/CONTPEAKS/CONTPEAKS_{}_{}_LOG.csv'
+ef = ContinuousPeaksEvaluationFunction(T)
+odd = DiscreteUniformDistribution(ranges)
+nf = DiscreteChangeOneNeighbor(ranges)
+mf = DiscreteChangeOneMutation(ranges)
+cf = SingleCrossOver()
+df = DiscreteDependencyTree(.1, ranges)
+hcp = GenericHillClimbingProblem(ef, odd, nf)
+gap = GenericGeneticAlgorithmProblem(ef, odd, mf, cf)
+pop = GenericProbabilisticOptimizationProblem(ef, odd, df)
+
+# RHC
+for t in range(numTrials):
+    fname = outfile.format('RHC', str(t + 1))
+    with open(fname, 'w') as f:
+        f.write('iterations,fitness,time,fevals\n')
+    ef = ContinuousPeaksEvaluationFunction(T)
+    odd = DiscreteUniformDistribution(ranges)
+    nf = DiscreteChangeOneNeighbor(ranges)
+    hcp = GenericHillClimbingProblem(ef, odd, nf)
+    rhc = RandomizedHillClimbing(hcp)
+    fit = FixedIterationTrainer(rhc, 10)
+    times = [0]
+    for i in range(0, maxIters, 10):
+        start = time.clock()
+        fit.train()
+        elapsed = time.clock() - start
+        times.append(times[-1] + elapsed)
+        fevals = ef.fevals
+        score = ef.value(rhc.getOptimal())
+        ef.fevals -= 1
+        st = '{},{},{},{}\n'.format(i, score, times[-1], fevals)
+        print st
+        with open(fname, 'a') as f:
+            f.write(st)
+
+# SA
+for t in range(numTrials):
+    for CE in [0.15, 0.35, 0.55, 0.75, 0.95]:
+        fname = outfile.format('SA{}'.format(CE), str(t + 1))
+        with open(fname, 'w') as f:
+            f.write('iterations,fitness,time,fevals\n')
+        ef = ContinuousPeaksEvaluationFunction(T)
+        odd = DiscreteUniformDistribution(ranges)
+        nf = DiscreteChangeOneNeighbor(ranges)
+        hcp = GenericHillClimbingProblem(ef, odd, nf)
+        sa = SimulatedAnnealing(1E10, CE, hcp)
+        fit = FixedIterationTrainer(sa, 10)
+        times = [0]
+        for i in range(0, maxIters, 10):
+            start = time.clock()
+            fit.train()
+            elapsed = time.clock() - start
+            times.append(times[-1] + elapsed)
+            fevals = ef.fevals
+            score = ef.value(sa.getOptimal())
+            ef.fevals -= 1
+            st = '{},{},{},{}\n'.format(i, score, times[-1], fevals)
+            print st
+            with open(fname, 'a') as f:
+                f.write(st)
+
+# GA
+for t in range(numTrials):
+    for pop, mate, mutate in product([100], [50, 30, 10], [50, 30, 10]):
+        fname = outfile.format('GA{}_{}_{}'.format(pop, mate, mutate), str(t + 1))
+        with open(fname, 'w') as f:
+            f.write('iterations,fitness,time,fevals\n')
+        ef = ContinuousPeaksEvaluationFunction(T)
+        odd = DiscreteUniformDistribution(ranges)
+        nf = DiscreteChangeOneNeighbor(ranges)
+        mf = DiscreteChangeOneMutation(ranges)
+        cf = SingleCrossOver()
+        gap = GenericGeneticAlgorithmProblem(ef, odd, mf, cf)
+        ga = StandardGeneticAlgorithm(pop, mate, mutate, gap)
+        fit = FixedIterationTrainer(ga, 10)
+        times = [0]
+        for i in range(0, maxIters, 10):
+            start = clock()
+            fit.train()
+            elapsed = time.clock() - start
+            times.append(times[-1] + elapsed)
+            fevals = ef.fevals
+            score = ef.value(ga.getOptimal())
+            ef.fevals -= 1
+            st = '{},{},{},{}\n'.format(i, score, times[-1], fevals)
+            print st
+            with open(fname, 'a') as f:
+                f.write(st)
+
+# MIMIC
+for t in range(numTrials):
+    for samples, keep, m in product([100], [50], [0.1, 0.3, 0.5, 0.7, 0.9]):
+        fname = outfile.format('MIMIC{}_{}_{}'.format(samples, keep, m), str(t + 1))
+        with open(fname, 'w') as f:
+            f.write('iterations,fitness,time,fevals\n')
+        ef = ContinuousPeaksEvaluationFunction(T)
+        odd = DiscreteUniformDistribution(ranges)
+        nf = DiscreteChangeOneNeighbor(ranges)
+        mf = DiscreteChangeOneMutation(ranges)
+        cf = SingleCrossOver()
+        gap = GenericGeneticAlgorithmProblem(ef, odd, mf, cf)
+        df = DiscreteDependencyTree(m, ranges)
+        pop = GenericProbabilisticOptimizationProblem(ef, odd, df)
+        mimic = MIMIC(samples, keep, pop)
+        fit = FixedIterationTrainer(mimic, 10)
+        times = [0]
+        for i in range(0, maxIters, 10):
+            start = clock()
+            fit.train()
+            elapsed = time.clock() - start
+            times.append(times[-1] + elapsed)
+            fevals = ef.fevals
+            score = ef.value(mimic.getOptimal())
+            ef.fevals -= 1
+            st = '{},{},{},{}\n'.format(i, score, times[-1], fevals)
+            print st
+            with open(fname, 'a') as f:
+                f.write(st)
